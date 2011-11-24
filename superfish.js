5d6b4145e02c7aed001f2fc80a5ed6d9c7fafd30
Mehrpadin <mehrpadin@619294.no-reply.drupal.org>
2011-11-24 19:14:50 +0330

    Version 1.9-beta4 Signed-off-by: Mehrpadin <mehrpadin@619294.no-reply.drupal.org>

--- superfish.js
+++ superfish.js
@@ -1,30 +1,30 @@
 (function ($) {
-	/**
+  /**
    * The Superfish Drupal Behavior to apply the Superfish jQuery plugin to lists.
    */
-	Drupal.behaviors.superfish = function (context) {
-		// Take a look at each list to apply Superfish to.
-		$.each(Drupal.settings.superfish || {}, function(index, options) {
-			// Process all Superfish lists.
-			var	list = $('#superfish-' + options.id, context);
-			// Apply Superfish to the list.
-			list.superfish(options.sf);
+  Drupal.behaviors.superfish = function (context) {
+    // Take a look at each list to apply Superfish to.
+    $.each(Drupal.settings.superfish || {}, function(index, options) {
+      // Process all Superfish lists.
+      var  list = $('#superfish-' + options.id, context);
+      // Apply Superfish to the list.
+      list.superfish(options.sf);
 
-			// Check if we are to apply any plugins to it.
-			if (options.plugins || false) {
-				if (options.plugins.touchscreen || false) {
-					list.sftouchscreen();
-				}
-				if (options.plugins.supposition || false) {
-					list.supposition();
-				}
-				if (options.plugins.bgiframe || false) {
-					list.find('ul').bgIframe({opacity:false});
-				}
-				if (options.plugins.supersubs || false) {
-					list.supersubs(options.plugins.supersubs);
-				}
-			}
-		});
-	};
+      // Check if we are to apply any plugins to it.
+      if (options.plugins || false) {
+        if (options.plugins.touchscreen || false) {
+          list.sftouchscreen();
+        }
+        if (options.plugins.supposition || false) {
+          list.supposition();
+        }
+        if (options.plugins.bgiframe || false) {
+          list.find('ul').bgIframe({opacity:false});
+        }
+        if (options.plugins.supersubs || false) {
+          list.supersubs(options.plugins.supersubs);
+        }
+      }
+    });
+  };
 }(jQuery));
